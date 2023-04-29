3bac8f0177139f4a510e3bd12c22096c8bfbfefc
GLGIO7 <giovanelucas00@outlook.com>
2023-04-29 17:21:06 -0300

    git commit -m "devops: cloning repo to linux"

--- src/main.jsx
+++ src/main.jsx
@@ -7,3 +7,12 @@ const rootElement = document.getElementById("root");
 ReactDOM.createRoot(rootElement).render(
 	<App />
 );
+
+
+// Testing from Ubuntu
+// Got Git credentials from WIndows Git
+// Set Git credentials on Linux
+// cd / git clone
+// git add .
+// git commit -m "commit message"
+// git push origin main or git push -u
